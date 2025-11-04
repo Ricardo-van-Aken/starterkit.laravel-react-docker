5c4d1a1b26a5f8babb3a69272ac6b45e99eb9974
Zeepaardje <rvaken98@gmail.com>
2025-11-04 10:21:54 +0100

    temporarily turned off ssl for db clone to make local testing work

--- website/docker/img_laravel/scripts/clone_db_for_testing.sh
+++ website/docker/img_laravel/scripts/clone_db_for_testing.sh
@@ -13,9 +13,9 @@ if [ -f /run/secrets/mysql_app_db_database ]; then
 fi
 
 # Drop and recreate the test database
-mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" -e "DROP DATABASE IF EXISTS \`$DB_DATABASE_TESTING\`;"
-mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" -e "CREATE DATABASE \`$DB_DATABASE_TESTING\`;"
+mysql --protocol=TCP --ssl=0 -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" -e "DROP DATABASE IF EXISTS \`$DB_DATABASE_TESTING\`;"
+mysql --protocol=TCP --ssl=0 -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" -e "CREATE DATABASE \`$DB_DATABASE_TESTING\`;"
 
 # Dump only the main database schema (and optionally data) and import into test database
-mysqldump --no-tablespaces -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" "$DB_DATABASE" | \
-  mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" "$DB_DATABASE_TESTING"
+mysqldump --no-tablespaces --protocol=TCP --ssl=0 -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" "$DB_DATABASE" | \
+  mysql --protocol=TCP --ssl=0 -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME_TESTING" -p"$DB_PASSWORD_TESTING" "$DB_DATABASE_TESTING"
