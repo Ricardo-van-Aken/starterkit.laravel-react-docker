7c14ccf4e462e1b4d82ebc837c38afb1d74dbcf5
Joël Buter <jtbuter@gmail.com>
2025-11-04 13:12:30 +0100

    testing terraform apply

--- deployment/terraform/environments/common/main.tf
+++ deployment/terraform/environments/common/main.tf
@@ -8,15 +8,15 @@ module "bucket" {
   depends_on = [module.project]
 }
 
-module "droplet" {
-  source       = "../../modules/droplet"
-  project_id   = module.project.id
-  ssh_key_name = var.ssh_key_name
-  depends_on   = [module.project]
-}
+# module "droplet" {
+#   source       = "../../modules/droplet"
+#   project_id   = module.project.id
+#   ssh_key_name = var.ssh_key_name
+#   depends_on   = [module.project]
+# }
 
-module "database" {
-  source     = "../../modules/database"
-  project_id = module.project.id
-  depends_on = [module.project]
-}
+# module "database" {
+#   source     = "../../modules/database"
+#   project_id = module.project.id
+#   depends_on = [module.project]
+# }
