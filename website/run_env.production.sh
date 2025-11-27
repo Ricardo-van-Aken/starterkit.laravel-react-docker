132c59a726f12d28a749c906e77ed193c31a72de
Joël Buter <jtbuter@gmail.com>
2025-11-27 12:54:02 +0100

    attempting to resolve certificate issue

---
+++ website/run_env.production.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ENV_FILE="docker/.env.production"
+PROFILE="production"
+COMPOSE_FILE="docker/docker-compose.production.yml"
+
+# Remove all volumes associated with this compose file (clean start)
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile all down -v
+
+# # Run docker compose with the selected .env file and profile
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE run --rm --build certbot init
+
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE up --build -d
