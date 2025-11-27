11b7627b66d038ddae5ac754f2974ebd84e7e12e
Joël Buter <jtbuter@gmail.com>
2025-11-27 13:36:50 +0100

    trying to get deployment script to work

--- website/run_env.production.sh
+++ website/run_env.production.sh
@@ -5,9 +5,9 @@ PROFILE="production"
 COMPOSE_FILE="docker/docker-compose.production.yml"
 
 # # Remove all volumes associated with this compose file (clean start)
-# docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile all down -v
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile all down -v
 
 # # Run docker compose with the selected .env file and profile
-# docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE run --rm --build certbot init --staging
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE run --rm --build certbot init --staging
 
 docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE up --build -d
