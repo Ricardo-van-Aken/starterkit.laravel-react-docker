0c177b6f5557d8296ceb4a9c89a659dac5719ea9
Joël Buter <jtbuter@gmail.com>
2025-11-27 14:46:26 +0100

    who decided that

--- website/run_env.production.sh
+++ website/run_env.production.sh
@@ -4,10 +4,12 @@ ENV_FILE="docker/.env.production"
 PROFILE="production"
 COMPOSE_FILE="docker/docker-compose.production.yml"
 
-# # Remove all volumes associated with this compose file (clean start)
-docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile all down -v
+# Remove all volumes associated with this compose file (clean start)
+docker rm docker_app_files
 
-# # Run docker compose with the selected .env file and profile
-docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE run --rm --build certbot init
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE pull
 
-docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE up --build -d
+# Run docker compose with the selected .env file and profile
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE run --rm certbot init
+
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE up -d
