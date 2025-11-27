431ffa3bec4e04f0f7c30fdd74999b6539d4e5d8
Joël Buter <jtbuter@gmail.com>
2025-11-27 14:14:21 +0100

    just run without staging ssl

--- website/run_env.production.sh
+++ website/run_env.production.sh
@@ -8,6 +8,6 @@ COMPOSE_FILE="docker/docker-compose.production.yml"
 docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile all down -v
 
 # # Run docker compose with the selected .env file and profile
-docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE run --rm --build certbot init --staging
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE run --rm --build certbot init
 
 docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE up --build -d
