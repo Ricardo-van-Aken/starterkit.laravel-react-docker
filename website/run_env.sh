7c5ac968fd9d30cd1efc28e4c38ca3e3a673e728
Joël Buter <jtbuter@gmail.com>
2025-12-01 09:22:01 +0100

    get rid of more hardcoded environment values

--- website/run_env.sh
+++ website/run_env.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # This script runs docker-compose.yml with a selected profile and .env file.
-# Usage: ./run_env_profile.sh [dev-volume|dev-bindmount|fake-prod|production]
+# Usage: ./run_env_profile.sh [dev-volume|dev-bindmount|fake-prod]
 
 # Get the mode from the first argument
 MODE="$1"
 
 # Show usage if no mode is provided
 if [ -z "$MODE" ]; then
-  echo "Usage: $0 [dev-volume|dev-bindmount|fake-prod|production]"
+  echo "Usage: $0 [dev-volume|dev-bindmount|fake-prod]"
   exit 1
 fi
 
@@ -32,29 +32,19 @@ case "$MODE" in
     ENV_FILE="docker/.env.testing"
     PROFILE="testing"
     ;;
-  production)
-    # For simulating production/testing
-    ENV_FILE="docker/.env.production"
-    PROFILE="production"
-    COMPOSE_FILE="docker/docker-compose.production.yml"
-    ;;
   *)
     # Invalid mode provided
     echo "Invalid mode: $MODE"
-    echo "Valid options: dev-volume, dev-bindmount, fake-prod, production"
+    echo "Valid options: dev-volume, dev-bindmount, fake-prod"
     exit 1
     ;;
 esac
 
 # Build the base laravel application image if it doesn't exist or Dockerfile.base has changed
-# docker build -f docker/img_laravel/Dockerfile.laravel-base -t local/starterkit.laravel-react-docker:laravel-base.latest .
+docker build -f docker/img_laravel/Dockerfile.laravel-base -t local/starterkit.laravel-react-docker:laravel-base.latest .
 
 # Remove all volumes associated with this compose file (clean start)
 docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile all down -v
 
-# # Run docker compose with the selected .env file and profile
-# docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE up -d --build
-docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE run --rm certbot init --staging
-# docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE down
-
-docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE up -d
+# Run docker compose with the selected .env file and profile
+docker compose -f $COMPOSE_FILE --env-file $ENV_FILE --profile $PROFILE up -d --build
