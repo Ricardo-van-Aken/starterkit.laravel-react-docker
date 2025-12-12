b7f3e1b31aa7fe93eeab840befdfbb0842ccde6c
Joël Buter <jtbuter@gmail.com>
2025-12-12 21:31:32 +0100

    testing planning step

--- deployment/terraform/environments/common/main.tf
+++ deployment/terraform/environments/common/main.tf
@@ -3,19 +3,19 @@ module "bucket" {
   project_id = var.project_id
 }
 
-module "droplet" {
-  source       = "../../modules/droplet"
-  project_id   = var.project_id
-  ssh_key_name = var.ssh_key_name
-}
+# module "droplet" {
+#   source       = "../../modules/droplet"
+#   project_id   = var.project_id
+#   ssh_key_name = var.ssh_key_name
+# }
 
-module "database" {
-  source     = "../../modules/database"
-  project_id = var.project_id
-}
+# module "database" {
+#   source     = "../../modules/database"
+#   project_id = var.project_id
+# }
 
-module "domain" {
-  source       = "../../modules/domain"
-  droplet_ipv4 = module.droplet.ipv4
-  project_id   = var.project_id
-}
+# module "domain" {
+#   source       = "../../modules/domain"
+#   droplet_ipv4 = module.droplet.ipv4
+#   project_id   = var.project_id
+# }
