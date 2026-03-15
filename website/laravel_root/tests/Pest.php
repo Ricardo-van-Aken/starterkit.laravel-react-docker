f46382791d33b987ec48a3cd964dcc4756f333b0
Zeepaardje <rvaken98@gmail.com>
2026-03-15 13:54:06 +0100

    removed accidental change in pest

--- website/laravel_root/tests/Pest.php
+++ website/laravel_root/tests/Pest.php
@@ -11,7 +11,8 @@
 |
 */
 
-uses(Tests\TestCase::class, Illuminate\Foundation\Testing\RefreshDatabase::class)
+pest()->extend(Tests\TestCase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
 /*
