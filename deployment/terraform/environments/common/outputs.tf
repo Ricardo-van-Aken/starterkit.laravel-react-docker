50c8c27079194120de461bb3ba5043068247a1ab
Ricardo van Aken <r.v.aken@protonmail.com>
2026-04-23 17:31:13 +0200

    added automatic secrets files generation to deployment workflow

--- deployment/terraform/environments/common/outputs.tf
+++ deployment/terraform/environments/common/outputs.tf
@@ -1,3 +1,24 @@
 output "droplet_ipv4" {
   value = module.droplet.ipv4
 }
+
+output "db_host" {
+  value = module.database.host
+}
+
+output "db_port" {
+  value = module.database.port
+}
+
+output "db_name" {
+  value = module.database.database
+}
+
+output "db_user" {
+  value = module.database.user
+}
+
+output "db_password" {
+  value     = module.database.password
+  sensitive = true
+}
--- deployment/terraform/modules/database/outputs.tf
+++ deployment/terraform/modules/database/outputs.tf
@@ -1,3 +1,24 @@
 output "urn" {
   value = digitalocean_database_cluster.my_cluster.urn
 }
+
+output "host" {
+  value = digitalocean_database_cluster.my_cluster.host
+}
+
+output "port" {
+  value = digitalocean_database_cluster.my_cluster.port
+}
+
+output "database" {
+  value = digitalocean_database_db.my_database.name
+}
+
+output "user" {
+  value = digitalocean_database_user.my_user.name
+}
+
+output "password" {
+  value     = digitalocean_database_user.my_user.password
+  sensitive = true
+}
